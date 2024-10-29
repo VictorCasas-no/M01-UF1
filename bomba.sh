9f83d5412953e2e4e6883650b2a48e440166b961
VictorCasas <victor.casas@enti.cat>
2024-10-29 14:37:35 +0100

    Ejercicio de fors

---
+++ bomba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ¿Desde qué número quieres que empiece? | cowsay
+#esto es un comentario
+read CANTIDAD
+
+#for NUMERO in $(seq 10)
+for NUMERO in `seq 1 $CANTIDAD | sort -hr`
+do
+	clear	
+	echo $NUMERO | cowsay
+	sleep 1
+done
+
+clear
+echo "BOOOOM!" | cowsay -d
